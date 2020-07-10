a4bedcba60327f1475f5a0d733db47ab5efe2f31
wajakondijos <47781133+wajakondijos@users.noreply.github.com>
2020-07-10 16:43:49 +0200

    commit

---
+++ js/scripts01.js
@@ -0,0 +1,73 @@
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-black";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-black", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+//Slideshow show
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2500); // Change image every 2,5 seconds
+}
+
+var j = 0;
+var txt = 'Krzysztof Kopciński';
+var speed = 50;
+function typeWriter() {
+  if (j < txt.length) {
+    document.getElementById("typewriter011").innerHTML += txt.charAt(j);
+    j++;
+    setTimeout(typeWriter, speed);
+  }
+	 }
+typeWriter();
+
+var k = 0;
+var txt02 = 'Pastry Chef';
+var speed = 50;
+function typeWriter02() {
+  if (k < txt02.length) {
+    document.getElementById("typewriter022").innerHTML += txt02.charAt(k);
+    k++;
+    setTimeout(typeWriter02, speed);
+  }
+	 }
+typeWriter02();
+	
